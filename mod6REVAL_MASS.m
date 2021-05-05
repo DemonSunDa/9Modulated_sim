1f94095e4c0b566adaa608ec3aa8a23e980e32e4
demon <demonsd98@outlook.com>
2021-05-05 17:59:35 +0800

    Dev done, bug with symmetry

--- mod6REVAL_MASS.m
+++ mod6REVAL_MASS.m
@@ -4,18 +4,25 @@
 % DEPENDENCY
 %   ACVMS
 % FILE OUTPUT
-%   ACVMS\IR_REP[num].mat
+%   ACVMS\IR_REVAL[num].mat
 
 
 clc;
 clear variables;
-load('.\ACVMS\IR_ACV10.mat');
+load('.\ACVMS\IR_ACV27.mat');
 
 % init
     % storage
-    reval(1 : size(val_corr, 1)) = struct('idc_eff', '',...
-        'R11', 0, 'R21', 0, 'corr1', 0,...
-        'R12', 0, 'R22', 0, 'corr2', 0);
+    reval(1 : sz_simresult(1), 1 : sz_simresult(2),...
+        1 : sz_simresult(3), 1 : sz_simresult(4),...
+        1 : sz_simresult(5), 1 : sz_simresult(6))...
+        = struct('idc_eff', '',...
+        'R11', [],...
+        'R21', [],...
+        'corr1', [],...
+        'R12', [],...
+        'R22', [],...
+        'corr2', []);
 % end init
 
 
@@ -28,65 +35,74 @@ load('.\ACVMS\IR_ACV10.mat');
     % plane_res = [stArr_simresult(1,1,11,11,4).val_corr(1, 1), stArr_simresult(1,1,11,11,4).val_corr(1, 2), 1];
     % plane_res_deg = stArr_simresult(1,1,11,11,4).val_corr(1, 3);
     % [eval_R, eval_phi, eval_tau] = get_R_value(plane_res, plane_sim, plane_res_deg, plane_sim_deg);
-    if ndims(val_corr) == 2
-        for ctr_res = 1 : size(val_corr, 1)
-            for ctr_d1 =
-                for ctr_d2 =
-                    for ctr_d3 =
-                        for ctr_d4 =
-                            for ctr_d5 =
-                                plane_res1 = [val_corr(ctr_res, 1), val_corr(ctr_res, 2), 1];
-                                plane_res1_deg = val_corr(ctr_res, 3);
-
-                                [evalR11, ~, ~] =...
-                                    get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
-                                [evalR21, ~, ~] =...
-                                    get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
-
-                                % store
-                                reval(ctr_res).R11 = evalR11;
-                                reval(ctr_res).R21 = evalR21;
-                                reval(ctr_res).corr1 = val_corr(ctr_res, 4);
-                            end
-                        end
-                    end
-                end
-            end
-
+    if ndims(stArr_simresult(1,1,1,1,1,1).val_corr) == 2
+        for ctr_d5 = 1 : sz_simresult(6)
+        for ctr_d4e = 1 : sz_simresult(5)
+        for ctr_d4 = 1 : sz_simresult(4)
+        for ctr_d3 = 1 : sz_simresult(3)
+        for ctr_d2e = 1 : sz_simresult(2)
+        for ctr_d2 = 1 : sz_simresult(1)
+        for ctr_res = 1 : size(stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).val_corr, 1)
+            reval(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).idc_eff = 'Result 1 effective.'
             
-        end
-    elseif ndims(val_corr) == 3
-        for ctr_res = 1 : size(val_corr, 1)
-            plane_res1 = [val_corr(ctr_res, 1, 1), val_corr(ctr_res, 2, 1), 1];
-            plane_res1_deg = val_corr(ctr_res, 3, 1);
-            plane_res2 = [val_corr(ctr_res, 1, 2), val_corr(ctr_res, 2, 2), 1];
-            plane_res2_deg = val_corr(ctr_res, 3, 2);
+            plane_sim1 = [stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg1.x_miller,...
+            stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg1.y_miller, 1];
+            plane_sim1_deg = stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg1.deg;
+            plane_sim2 = [stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg2.x_miller,...
+            stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg2.y_miller, 1];
+            plane_sim2_deg = stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).initg2.deg;
+            
+            plane_res1 = [stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).val_corr(ctr_res, 1), stArr_simresult(1,1,1,1,1,1).val_corr(ctr_res, 2), 1];
+            plane_res1_deg = stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).val_corr(ctr_res, 3);
 
             [evalR11, ~, ~] =...
                 get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
             [evalR21, ~, ~] =...
                 get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
-            [evalR12, ~, ~] =...
-                get_R_value(plane_res2, plane_sim1, plane_res2_deg, plane_sim1_deg);
-            [evalR22, ~, ~] =...
-                get_R_value(plane_res2, plane_sim2, plane_res2_deg, plane_sim2_deg);
 
             % store
-            reval(ctr_res).R11 = evalR11;
-            reval(ctr_res).R21 = evalR21;
-            reval(ctr_res).corr1 = val_corr(ctr_res, 4, 1);
-            reval(ctr_res).R12 = evalR12;
-            reval(ctr_res).R22 = evalR22;
-            reval(ctr_res).corr2 = val_corr(ctr_res, 4, 2);
-        end
+            reval(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).R11(ctr_res) = evalR11;
+            reval(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).R21(ctr_res) = evalR21;
+            reval(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).corr1(ctr_res) = stArr_simresult(ctr_d2, ctr_d2e, ctr_d3, ctr_d4, ctr_d4e, ctr_d5).val_corr(ctr_res, 4);
+        end % end res
+        end % end d2
+        end % end d2e
+        end % end d3
+        end % end d4
+        end % end d4e
+        end % end d5
+    elseif ndims(stArr_simresult(1,1,1,1,1,1).val_corr) == 3
+%         for ctr_res = 1 : size(val_corr, 1)
+%             plane_res1 = [val_corr(ctr_res, 1, 1), val_corr(ctr_res, 2, 1), 1];
+%             plane_res1_deg = val_corr(ctr_res, 3, 1);
+%             plane_res2 = [val_corr(ctr_res, 1, 2), val_corr(ctr_res, 2, 2), 1];
+%             plane_res2_deg = val_corr(ctr_res, 3, 2);
+% 
+%             [evalR11, ~, ~] =...
+%                 get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
+%             [evalR21, ~, ~] =...
+%                 get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
+%             [evalR12, ~, ~] =...
+%                 get_R_value(plane_res2, plane_sim1, plane_res2_deg, plane_sim1_deg);
+%             [evalR22, ~, ~] =...
+%                 get_R_value(plane_res2, plane_sim2, plane_res2_deg, plane_sim2_deg);
+% 
+%             % store
+%             reval(ctr_res).R11 = evalR11;
+%             reval(ctr_res).R21 = evalR21;
+%             reval(ctr_res).corr1 = val_corr(ctr_res, 4, 1);
+%             reval(ctr_res).R12 = evalR12;
+%             reval(ctr_res).R22 = evalR22;
+%             reval(ctr_res).corr2 = val_corr(ctr_res, 4, 2);
+%         end
     end
 % end process
 
 
 % OUTPUT save
-fprintf('ARCHIVING\n');
-str_acv = sprintf('.\\ACVMS\\IR_REVAL%d.mat', ctr_acv);
-save(str_acv, 'reval');
-%     clear all;
-fprintf('IR_SIM DONE\n\n');
+    fprintf('ARCHIVING\n');
+    str_acv = sprintf('.\\ACVMS\\IR_REVAL%d.mat', ctr_acv);
+    save(str_acv, 'reval');
+    % clear all;
+    fprintf('IR_REVAL DONE\n\n');
 % end output save
