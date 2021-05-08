4c0efea441067ad7ddaf6904a58b2e244b1c17d3
demon <demonsd98@outlook.com>
2021-05-08 21:44:12 +0800

    FSMETHOD_SINGLE dev done

--- mod6REVAL_SINGLE.m
+++ mod6REVAL_SINGLE.m
@@ -9,7 +9,7 @@
 
 clc;
 clear variables;
-load('.\ACVSG\IR_ACV3.mat');
+load('.\ACVSG\IR_ACV19.mat');
 
 
 % plane_a = [1, 1, 1];
@@ -25,13 +25,20 @@ load('.\ACVSG\IR_ACV3.mat');
     plane_sim2 = [initg2.x_miller, initg2.y_miller, 1];
     plane_sim2_deg = initg2.deg;
     % storage
-    reval = struct('idc_eff', '',...
-        'R11', zeros(1, size(val_corr, 1)),...
-        'R21', zeros(1 : size(val_corr, 1)),...
-        'corr1', zeros(1 : size(val_corr, 1)),...
-        'R12', zeros(1, size(val_corr, 1)),...
-        'R22', zeros(1 : size(val_corr, 1)),...
-        'corr2', zeros(1 : size(val_corr, 1)));
+    if ~exist('val_corr_g2', 'var')
+        reval = struct(...
+            'R11', [],...
+            'R21', [],...
+            'corr1', []);
+    else
+        reval = struct(...
+            'R11', [],...
+            'R21', [],...
+            'corr1', [],...
+            'R12', [],...
+            'R22', [],...
+            'corr2', []);
+    end
 % end init
 
 
@@ -39,46 +46,35 @@ load('.\ACVSG\IR_ACV3.mat');
     % two conditions:
     % without FSMETHOD =>> 1 ir result
     % with FSMethod =>> 2 ir result
-    if ndims(val_corr) == 2
-        reval.idc_eff = 'Result 1 effective.';
-        for ctr_res = 1 : size(val_corr, 1)
-            plane_res1 = [val_corr(ctr_res, 1), val_corr(ctr_res, 2), 1];
-            plane_res1_deg = val_corr(ctr_res, 3);
+    for ctr_res = 1 : size(val_corr, 1)
+        plane_res1 = [val_corr(ctr_res, 1), val_corr(ctr_res, 2), 1];
+        plane_res1_deg = val_corr(ctr_res, 3);
 
-            [evalR11, ~, ~] =...
-                get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
-            [evalR21, ~, ~] =...
-                get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
+        [evalR11, ~, ~] =...
+            get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
+        [evalR21, ~, ~] =...
+            get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
 
-            % store
-            reval.R11(ctr_res) = evalR11;
-            reval.R21(ctr_res) = evalR21;
-            reval.corr1(ctr_res) = val_corr(ctr_res, 4);
-        end
-    elseif ndims(val_corr) == 3
-        reval.idc_eff = 'Result 1 and 2 effective.';
-        for ctr_res = 1 : size(val_corr, 1)
-            plane_res1 = [val_corr(ctr_res, 1, 1), val_corr(ctr_res, 2, 1), 1];
-            plane_res1_deg = val_corr(ctr_res, 3, 1);
-            plane_res2 = [val_corr(ctr_res, 1, 2), val_corr(ctr_res, 2, 2), 1];
-            plane_res2_deg = val_corr(ctr_res, 3, 2);
+        % store
+        reval.R11(ctr_res) = evalR11;
+        reval.R21(ctr_res) = evalR21;
+        reval.corr1(ctr_res) = val_corr(ctr_res, 4);
+    end
+    
+    if exist('val_corr_g2', 'var')
+        for ctr_res = 1 : size(val_corr_g2, 1)
+            plane_res2 = [val_corr_g2(ctr_res, 1), val_corr_g2(ctr_res, 2), 1];
+            plane_res2_deg = val_corr_g2(ctr_res, 3);
 
-            [evalR11, ~, ~] =...
-                get_R_value(plane_res1, plane_sim1, plane_res1_deg, plane_sim1_deg);
-            [evalR21, ~, ~] =...
-                get_R_value(plane_res1, plane_sim2, plane_res1_deg, plane_sim2_deg);
             [evalR12, ~, ~] =...
                 get_R_value(plane_res2, plane_sim1, plane_res2_deg, plane_sim1_deg);
             [evalR22, ~, ~] =...
                 get_R_value(plane_res2, plane_sim2, plane_res2_deg, plane_sim2_deg);
 
             % store
-            reval.R11(ctr_res) = evalR11;
-            reval.R21(ctr_res) = evalR21;
-            reval.corr1(ctr_res) = val_corr(ctr_res, 4, 1);
             reval.R12(ctr_res) = evalR12;
             reval.R22(ctr_res) = evalR22;
-            reval.corr2(ctr_res) = val_corr(ctr_res, 4, 2);
+            reval.corr2(ctr_res) = val_corr_g2(ctr_res, 4);
         end
     end
 % end process
