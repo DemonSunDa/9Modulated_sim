b764e2700b6d0e8f8c1065ef6ace990c97cda36a
demon <demonsd98@outlook.com>
2021-05-16 01:04:41 +0800

    Symmetry developing

--- sym_deg.m
+++ sym_deg.m
@@ -1,8 +1,12 @@
-function [x_OUT, y_OUT, deg_OUT] = sym_deg(x_IN, y_IN, deg_IN)
+function [x_OUT, y_OUT, deg_OUT, ] = sym_deg(x1_IN, y1_IN, deg1_IN, x2_IN, y2_IN, deg2_IN)
+    if 
+    
+    end
+    
     if (x_IN == 0) && (y_IN == 0)
-        while deg_OUT
+        grain_temp = zeros()
             
-        end
+
     elseif (x_IN == 1)  && (y_IN == 1)
 
     end
