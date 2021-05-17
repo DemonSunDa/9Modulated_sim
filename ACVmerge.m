d90eee582bc141c5fa8002fddb15d08338fbc78f
demon <demonsd98@outlook.com>
2021-05-18 00:40:46 +0800

    ACV numbering refined

--- ACVmerge.m
+++ ACVmerge.m
@@ -9,8 +9,8 @@ load('.\ACVMS\ACV48_IR.mat');
 stArr_simresult3 = stArr_simresult;
 
 stArr_simresult = cat(6, stArr_simresult1, stArr_simresult2, stArr_simresult3);
-ctr_acv = '46-48_IR';
+ctr_acv = '46-48';
 sel.noise = 1 : 11;
 sz_simresult = size(stArr_simresult);
 
-save('.\ACVMS\ACV46-48_IR.mat', 'ctr_acv', 'SC', 'sel', 'stArr_simresult', 'sz_simresult', 'vec');
+save('.\ACVMS\ACV46-48.mat', 'ctr_acv', 'SC', 'sel', 'stArr_simresult', 'sz_simresult', 'vec');
